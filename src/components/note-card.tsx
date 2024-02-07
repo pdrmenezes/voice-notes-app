3319c5aebcff17ff40d46e320472b4303af4d5e1
pdrmenezes <pdrmenezes1@gmail.com>
2024-02-07 19:27:05 -0300

    feat: accesible card with focus-visible css prop

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,6 +1,6 @@
 export function NoteCard() {
   return (
-    <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
+    <button className="rounded-md bg-slate-800 p-5 text-left space-y-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
       <span className="text-sm font-medium text-slate-300">Há 2 dias</span>
       <p className="text-sm leading-6 text-slate-400">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo nesciunt maxime ipsam ipsum molestiae!
@@ -8,6 +8,6 @@ export function NoteCard() {
         rerum dolorem.
       </p>
       <div className="absolute bottom-0 left-0 right-0 h-1/2 rounded-md bg-gradient-to-t from-black/60 to-black/0 pointer-events-none"></div>
-    </div>
+    </button>
   );
 }
